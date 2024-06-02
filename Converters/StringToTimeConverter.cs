964119c67cdf44b85cb076178839f87a776ea07a
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-02 03:16:50 +0100

    update

---
+++ Converters/StringToTimeConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace FrontEnd.Converters
+{
+    /// <summary>
+    /// Use this converter to deal with TimeSpan.<para/>
+    /// For Example in your xaml file:
+    /// <code>
+    /// &lt;fr:Text Grid.Column="4" Text="{Binding TOA, Converter={StaticResource TimeBox}}"/>
+    /// </code>
+    /// TOA is a TimeSpan property.
+    /// </summary>
+    public class StringToTimeConverter : IValueConverter
+    {
+        private const string format = "h:mm tt";
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan time) 
+            {
+                DateTime date = DateTime.Today.Add(time);
+                return date.ToString(format, CultureInfo.InvariantCulture);
+            }
+            return value;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                DateTime parsedDateTime;
+                if (DateTime.TryParseExact(str, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDateTime))
+                {
+                    TimeSpan timeSpan = parsedDateTime.TimeOfDay;
+                    return timeSpan;
+                }
+                return value;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+
+    }
+}
