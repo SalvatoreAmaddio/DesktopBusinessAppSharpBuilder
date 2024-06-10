5a7b21601cc376f212bb78d04864b1d4f5f073c1
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-10 22:04:57 +0100

    upate

--- Converters/HeaderFilterConverter.cs
+++ Converters/HeaderFilterConverter.cs
@@ -5,23 +5,33 @@ namespace FrontEnd.Converters
 {
     public class HeaderFilterConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is bool yesNo) 
+            switch (value)
             {
-                return (yesNo) ? "Yes" : "No";
+                case bool yesNo:
+                    return yesNo ? "Yes" : "No";
+                case DateTime date:
+                    return date.ToString("dd/MM/yyyy", culture);
+                default:
+                    return value?.ToString();
             }
-            if (value is DateTime date)
-                return date.ToString("dd/MM/yyyy");
-            return value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is not string str) return value;
-            if (str.Equals("Yes")) return true;
-            if (str.Equals("No")) return false;
-            return DateTime.Parse(str);
+            if (value is not string str)
+                return value;
+
+            if (str.Equals("Yes", StringComparison.OrdinalIgnoreCase))
+                return true;
+            if (str.Equals("No", StringComparison.OrdinalIgnoreCase))
+                return false;
+
+            if (DateTime.TryParse(str, culture, DateTimeStyles.None, out DateTime date))
+                return date;
+
+            return value;
         }
     }
 }
