21f826bcf57caf73fea35259f45144b57beec9b1
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-26 21:46:27 +0100

    update

---
+++ Behaviours/ScrollSynchronizer.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace FrontEnd.Behaviours
+{
+    public static class ScrollSynchronizer
+    {
+        public static readonly DependencyProperty SynchronizeWithProperty =
+            DependencyProperty.RegisterAttached("SynchronizeWith", typeof(ScrollViewer), typeof(ScrollSynchronizer), new PropertyMetadata(null, OnSynchronizeWithChanged));
+
+        public static ScrollViewer GetSynchronizeWith(DependencyObject obj)
+        {
+            return (ScrollViewer)obj.GetValue(SynchronizeWithProperty);
+        }
+
+        public static void SetSynchronizeWith(DependencyObject obj, ScrollViewer value)
+        {
+            obj.SetValue(SynchronizeWithProperty, value);
+        }
+
+        private static void OnSynchronizeWithChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ScrollViewer scrollViewer && e.NewValue is ScrollViewer targetScrollViewer)
+            {
+                scrollViewer.ScrollChanged += (sender, args) =>
+                {
+                    if (args.HorizontalChange != 0)
+                    {
+                        targetScrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset);
+                    }
+                };
+
+                targetScrollViewer.ScrollChanged += (sender, args) =>
+                {
+                    if (args.HorizontalChange != 0)
+                    {
+                        scrollViewer.ScrollToHorizontalOffset(targetScrollViewer.HorizontalOffset);
+                    }
+                };
+            }
+        }
+    }
+}
